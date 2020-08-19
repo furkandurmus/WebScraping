16292f50f91eb216bfbc2a32563296e2c429cb56
furkandurmus <furkandurmus282@gmail.com>
2020-08-19 21:06:13 +0300

    modified

--- ImdbWebScrapingProject.py
+++ ImdbWebScrapingProject.py
@@ -19,10 +19,10 @@ response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 
 
-movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
+movies = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
 
 
-first = movie_containers[0]
+first = movies[0]
 
 print("First Films Name:", first.h3.a.get_text())
 print("First Films Rating:", first.find('div', class_="inline-block ratings-imdb-rating").get_text().replace('\n',''))
@@ -30,18 +30,18 @@ print("First Films Rating:", first.find('div', class_="inline-block ratings-imdb
 #get the name of the films, ratings, metascore, votings and release year. 
 #Metascore parameter is not valid for all the data so we needed to check 
 #if it is null or not in a for loop.
-films = [i.h3.a.get_text() for i in movie_containers]
-ratings = [i.find('strong').get_text() for i in movie_containers]
+films = [i.h3.a.get_text() for i in movies]
+ratings = [i.find('strong').get_text() for i in movies]
 metascore = []
-for i in movie_containers:
+for i in movies:
     if i.find('span', class_="metascore") == None:
         metascore.append(None)
     else:
         metascore.append(i.find('span', class_="metascore").get_text())
 
 # [i.find('span', class_="metascore favorable").get_text() for i in movie_containers if i.find('span', class_="metascore favorable") is None i=None]
-release_year = [i.find('span', class_="lister-item-year text-muted unbold").get_text() for i in movie_containers]
-votings=[i.find('span', attrs = {'name':'nv'}).text for i in movie_containers]
+release_year = [i.find('span', class_="lister-item-year text-muted unbold").get_text() for i in movies]
+votings=[i.find('span', attrs = {'name':'nv'}).text for i in movies]
 
 # from the collected data we created a simple data frame object.
 data = pd.DataFrame({"Film Name":films, "Votes":votings, "Ratings":ratings, "Metascore":metascore, "Release Year":release_year})
